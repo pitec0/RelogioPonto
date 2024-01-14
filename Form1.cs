1416bf9bae5246b46449f632c4650a9d7e30b597
pitec0 <igorgustavodoamaral94@gmail.com>
2024-01-14 06:55:51 -0300

    Code cleaned.

--- Form1.cs
+++ Form1.cs
@@ -13,96 +13,92 @@ using Org.BouncyCastle.Utilities;
 
 namespace Relogio_Ponto
 {
-    public partial class Form1 : Form
+  public partial class Form1 : Form
+  {
+    public Form1()
     {
-        public Form1()
-        {
-            InitializeComponent();
-        }
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-
-        }
-
-       
-        private void btnEntrada_Click(object sender, EventArgs e)
-        {
-            DateTime now = DateTime.Now;
-            if (txtUsuario.Text == "")
-            {
-                MessageBox.Show("Preencha o campo Usuário.");
-                txtUsuario.Focus();
-                return;
-            }
-            if (txtSenha.Text == "")
-            {
-                MessageBox.Show("Preencha o campo Senha.");
-                txtSenha.Focus();
-                return;
-            }
-            string nome = txtUsuario.Text;
-            string senha = txtSenha.Text;
-
-            if (FormCadastro.Login(nome, senha))
-            {
-                MessageBox.Show("Entrada realizada em " + now);
-            }
-            else
-            {
-                MessageBox.Show("Login ou senha incorreto!");
-                txtUsuario.Text = "";
-                txtSenha.Text = "";
-                txtUsuario.Focus();
-                this.Close();
-            }
-                txtUsuario.Text = string.Empty;
-                txtSenha.Text = string.Empty;
-            
-
-
-            //fazer envio para banco de dados da hora de entrada
-        }
-
-        private void btnSaida_Click(object sender, EventArgs e)
-        {
-            DateTime now = DateTime.Now;
-            if (txtUsuario.Text == " ")
-            {
-                MessageBox.Show("Preencha o campo Usuário.");
-                txtUsuario.Focus();
-                return;
-            }
-            if (txtSenha.Text == " ")
-            {
-                MessageBox.Show("Preencha o campo Senha.");
-                txtSenha.Focus();
-                return;
-            }
-            string nome = txtUsuario.Text;
-            string senha = txtSenha.Text;
-            if (FormCadastro.Login(nome, senha))
-            {
-                MessageBox.Show("Saida realizada em " + now);
-            }
-            else
-            {
-                MessageBox.Show("Login ou senha incorreto!");
-                txtUsuario.Text = "";
-                txtSenha.Text = "";
-                txtUsuario.Focus();
-                this.Close();
-            }
-            txtUsuario.Text = string.Empty;
-            txtSenha.Text = string.Empty;
-
-            //fazer envio para banco de dados da hora de saida
-        }
-
-        private void btnCadastrar_Click(object sender, EventArgs e)
-        {
-            FormCadastro newFuncionario = new FormCadastro();
-            newFuncionario.Show();
-        }
+      InitializeComponent();
     }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+    }
+
+    private void btnEntrada_Click(object sender, EventArgs e)
+    {
+      DateTime now = DateTime.Now;
+      if (txtUsuario.Text == "")
+      {
+        MessageBox.Show("Preencha o campo Usuário.");
+        txtUsuario.Focus();
+        return;
+      }
+      if (txtSenha.Text == "")
+      {
+        MessageBox.Show("Preencha o campo Senha.");
+        txtSenha.Focus();
+        return;
+      }
+      string nome = txtUsuario.Text;
+      string senha = txtSenha.Text;
+
+      if (FormCadastro.Login(nome, senha))
+      {
+        MessageBox.Show("Entrada realizada em " + now);
+      }
+      else
+      {
+        MessageBox.Show("Login ou senha incorreto!");
+        txtUsuario.Text = "";
+        txtSenha.Text = "";
+        txtUsuario.Focus();
+        this.Close();
+      }
+      txtUsuario.Text = string.Empty;
+      txtSenha.Text = string.Empty;
+
+      //fazer envio para banco de dados da hora de entrada
+    }
+
+    private void btnSaida_Click(object sender, EventArgs e)
+    {
+      DateTime now = DateTime.Now;
+      if (txtUsuario.Text == " ")
+      {
+        MessageBox.Show("Preencha o campo Usuário.");
+        txtUsuario.Focus();
+        return;
+      }
+      if (txtSenha.Text == " ")
+      {
+        MessageBox.Show("Preencha o campo Senha.");
+        txtSenha.Focus();
+        return;
+      }
+      string nome = txtUsuario.Text;
+      string senha = txtSenha.Text;
+      if (FormCadastro.Login(nome, senha))
+      {
+        MessageBox.Show("Saida realizada em " + now);
+      }
+      else
+      {
+        MessageBox.Show("Login ou senha incorreto!");
+        txtUsuario.Text = "";
+        txtSenha.Text = "";
+        txtUsuario.Focus();
+        this.Close();
+      }
+      txtUsuario.Text = string.Empty;
+      txtSenha.Text = string.Empty;
+
+      //fazer envio para banco de dados da hora de saida
+    }
+
+    private void btnCadastrar_Click(object sender, EventArgs e)
+    {
+      FormCadastro newFuncionario = new FormCadastro();
+      newFuncionario.Show();
+    }
+  }
 }
--- ValidacaoLogin.cs
+++ ValidacaoLogin.cs
@@ -1,45 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Relogio_Ponto
+﻿namespace Relogio_Ponto
 {
-    internal partial class FormCadastro
+  internal partial class FormCadastro
+  {
+    private static Usuario[] usuarios =
     {
+      //usuario teste
+      new Usuario()
+      {
+        UserName = "igor", Senha = "igor"
+      },
+    };
 
-        private static Usuario[] usuarios =
-        {
-            //usuario teste
-            new Usuario()
-            {
-                UserName = "igor", Senha = "igor"
-            },
+    private static Usuario _userLogado = null;
 
-        };
-
-        private static Usuario _userLogado = null;
-
-        public static Usuario UsuarioLogado
-        {
-            get { return _userLogado; }
-            private set { _userLogado = value; }
-        }
-
-        public static bool Login(string nome, string senha)
-        {
-            foreach (Usuario usuario in usuarios)
-            {
-                if (usuario.UserName == nome && usuario.Senha == senha)
-                {
-                    UsuarioLogado = usuario;
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        
+    public static Usuario UsuarioLogado
+    {
+      get { return _userLogado; }
+      private set { _userLogado = value; }
     }
+
+    public static bool Login(string nome, string senha)
+    {
+      foreach (Usuario usuario in usuarios)
+      {
+        if (usuario.UserName == nome && usuario.Senha == senha)
+        {
+          UsuarioLogado = usuario;
+          return true;
+        }
+      }
+      return false;
+    }
+  }
 }
