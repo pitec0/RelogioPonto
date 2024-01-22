972201ca248a860e15d8c1f3b50e09cc1e530a22
pitec0 <igorgustavodoamaral94@gmail.com>
2024-01-22 20:09:13 -0300

    teste

--- Form1.cs
+++ Form1.cs
@@ -10,13 +10,13 @@ namespace Relogio_Ponto
     {
     }
     //criar parametro no db para nome de ENTRADA E SAIDA PARA PODER FAZER FILTAGEM DE QUANDO ENTROU E QUANDO SAIU.
-    //criar parametro no db para nome de ENTRADA E SAIDA
-    //criar parametro no db para nome de ENTRADA E SAIDA
-    //criar parametro no db para nome de ENTRADA E SAIDA
-    //criar parametro no db para nome de ENTRADA E SAIDA
-    //criar parametro no db para nome de ENTRADA E SAIDA
-    //criar parametro no db para nome de ENTRADA E SAIDA
     //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA.
+    //criar parametro no db para nome de ENTRADA E SAIDA..
 
     private void btnEntrada_Click(object sender, EventArgs e)
     {
